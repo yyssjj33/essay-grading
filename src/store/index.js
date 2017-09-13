0a849a1dbd9a8184ef16a51616cbceeeed724ff1
jiyang <jiyang@rubiconproject.com>
2017-09-13 16:39:32 -0700

    add localstorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,37 @@
 import { createStore } from 'redux';
 import rootReducer from '../reducers';
+import _ from 'lodash';
+let persistedState;
+const ls = localStorage.getItem("state")
+if (ls) {
+  persistedState = JSON.parse(ls);
+} else {
+  persistedState = {
+    "tiyibiaoda": {
+      "zhibaiOrHanxu": null,
+      "zhengqueOrCuowu": null,
+      "tiyifangshi": null,
+      "tiyifangshiQita": "",
+      "zhibaiTiyijuzi": "",
+      "hanxuTiyijuzi": "",
+      "duanshu": 0,
+      "hangshu": 0
+    },
+    "shijianzhicheng": {},
+    "daolizhicheng": {},
+    "quanwentiyizhicheng": null,
+    "yuyanbiaoda": {
+      "jiegou": null,
+      "silu": null,
+      "yuyan": null,
+      "zonghe": null
+    }
+  }
+}
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, persistedState);
 window.store = store;
+store.subscribe(_.debounce(() => {
+  localStorage.setItem("state", JSON.stringify(store.getState()))
+}, 500))
 export default store;
